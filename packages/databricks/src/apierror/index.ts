04062f065d846e09198fdb6371148ec66c046993
Parth Bansal <parth.bansal@databricks.com>
2026-02-26 13:59:50 +0100

    Add API error detail types with Zod-based parsing (#11)
    * Add ErrorDetails Message and it's loading
    
    * update
    
    * update

--- packages/databricks/src/apierror/index.ts
+++ packages/databricks/src/apierror/index.ts
@@ -3,3 +3,20 @@
  *
  * @packageDocumentation
  */
+
+export type {
+  ErrorDetails,
+  ErrorInfo,
+  RequestInfo,
+  RetryInfo,
+  DebugInfo,
+  QuotaFailure,
+  QuotaFailureViolation,
+  PreconditionFailure,
+  PreconditionFailureViolation,
+  BadRequest,
+  BadRequestFieldViolation,
+  ResourceInfo,
+  Help,
+  HelpLink,
+} from './details';
