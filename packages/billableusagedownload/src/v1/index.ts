f4bdcae78dceb9435ef5d83d6157a1b0eeabc195
Parth Bansal <parth.bansal@databricks.com>
2026-05-29 14:02:01 +0200

    Update SDK to latest State of Generator (#168)
    ## 🥞 Stacked PR
    Use this [link](https://github.com/databricks/sdk-js/pull/168/files) to
    review incremental changes.
    -
    [**stack/credentials-discovery-2**](https://github.com/databricks/sdk-js/pull/168)
    [[Files changed](https://github.com/databricks/sdk-js/pull/168/files)]
    
    ---------
    
    ## Summary
    This PR updates the SDK to the latest state of the Generator.
    
    ## Why
    
    Because of upstream changes in the code generator.
    
    
    ## How is this tested?
    
    Not tested — this is generated code. The generator itself is tested
    upstream.

--- packages/billableusagedownload/src/v1/index.ts
+++
@@ -1,5 +0,0 @@
-// Code generated from API definition by Databricks SDK Generator. DO NOT EDIT.
-
-export {BillableusagedownloadClient} from './client';
-
-export type {DownloadRequest, DownloadResponse} from './model';
